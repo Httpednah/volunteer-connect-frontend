4a10787d8830f08acfcd572e9864c115fd0035e2
Peter Xervi <xervipeter@gmail.com>
2026-01-16 10:16:43 +0300

    docs: add comments to App.jsx

--- volunteer-connect-frontend/src/App.jsx
+++ volunteer-connect-frontend/src/App.jsx
@@ -1,4 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+/**
+ * This is the main router file. 
+ * It maps URLs (like /login) to specific Page components.
+ */
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -6,9 +10,9 @@ import Opportunities from "./pages/Opportunities";
 import Organization from "./pages/Organization";
 import VolunteerDashboard from "./pages/VolunteerDashboard";
 
-export default function App() {
+function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
@@ -17,6 +21,8 @@ export default function App() {
         <Route path="/organization" element={<Organization />} />
         <Route path="/dashboard" element={<VolunteerDashboard />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
+
+export default App;
