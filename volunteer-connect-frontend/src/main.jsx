563fd2ab2d435938ba9554c5d7e068a16c26b99a
Peter Xervi <xervipeter@gmail.com>
2026-01-16 10:16:43 +0300

    docs: add comments to main.jsx

--- volunteer-connect-frontend/src/main.jsx
+++ volunteer-connect-frontend/src/main.jsx
@@ -1,11 +1,19 @@
+/**
+ * Entry point of the React application.
+ * Here we render the App component into the HTML 'root' element
+ * and wrap it with necessary Global Providers (like ThemeProvider).
+ */
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles.css";
+import "./styles.css"; // Load our Tailwind and custom CSS
+import { ThemeProvider } from "@/components/theme-provider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+     {/* ThemeProvider makes Dark/Light mode available everywhere */}
+     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+        <App />
+     </ThemeProvider>
   </React.StrictMode>
 );
-
