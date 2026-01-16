cb8b295f331e1659303b1737a431b04623124d8e
Ednah wairimu kimani <kimaniednah@gmail.com>
2026-01-16 16:47:09 +0300

    volunteer page

---
+++ volunteer-connect-frontend/src/pages/VolunteerDashboard.jsx
@@ -0,0 +1,8 @@
+export default function VolunteerDashboard() {
+  return (
+    <div>
+      <h2>Volunteer Dashboard</h2>
+      <p>Apply to opportunities and track applications</p>
+    </div>
+  );
+}
